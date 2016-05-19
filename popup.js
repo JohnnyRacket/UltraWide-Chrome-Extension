e4016502de96bd8b91ffb4a1688c4665f9e5e5df
johnnyracket <john.randolph.harrison@gmail.com>
2016-05-19 18:31:26 -0400

    first basically fully working version, though still some hardcoding to remove and I want to enable the slider and the commands

--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
 var SystemInformation = SystemInformation || {};
+SystemInformation.scrollBarWidth = 17; //seems to be standard and I cant find a way to calc it otf yet
 
 function getViewportSize(tabs){
-  SystemInformation.viewportHeight = tabs[0].height;
+  SystemInformation.viewportHeight = tabs[0].height - SystemInformation.scrollBarWidth;
   chrome.system.display.getInfo(setScreenSize);
 }
 
@@ -24,17 +25,18 @@ function downsizeScreen(window){
   console.log("in downsizeScreen");
   windowId = window.id;
   console.log(window);
-//i need to replace this with a way to calulate the biggest 1080p ratio possible later
+//calulate the biggest 16:9 ratio possible later
 //first start to find dom height
 //then domHeight * 16/9 = desired dom width
 //will have to add some to account for scroll bars and myabe other pixels
 console.log("screen height is: " + SystemInformation.viewportHeight);
-  SystemInformation.viewportWidth = Math.ceil(SystemInformation.viewportHeight * 16/9); //right now only supports landscape things
+  SystemInformation.viewportWidth = Math.floor(((SystemInformation.viewportHeight) * 16/9)); //right now only supports landscape things... 17 is scrollbar size
+  SystemInformation.browserWidth = SystemInformation.viewportWidth + SystemInformation.scrollBarWidth*2; // still nto sure why its scrollbar*2 and not just a single
   console.log("adjusted width = " + SystemInformation.viewportWidth);
   updateInfo = {
-    left: SystemInformation.screenWidth - SystemInformation.viewportWidth + 10, //dunno why its 10 pixels off yet
+    left: SystemInformation.screenWidth - SystemInformation.browserWidth + 10, //dunno why its 10 pixels off yet
     top: 0,
-    width: SystemInformation.viewportWidth,
+    width: SystemInformation.browserWidth,
     height: SystemInformation.browserHeight + 10//, //right now its still 10 px off here as well
     //state: "fullscreen"
   }
--- websites/kissanime.js
+++ websites/kissanime.js
@@ -3,9 +3,14 @@ console.log("kissanime script loaded");
 function bloatVideo(width, height){
   console.log("bloating video, here i will need to access the page and modify the html!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
   var videoPlayer = document.getElementById("my_video_1");
-  videoPlayer.style.height = height - getScrollBarSize() + "px"; // the 20 is for scrollbar
-  videoPlayer.style.width = width - getScrollBarSize()  - 14 + "px";
+  videoPlayer.style.height = height + "px"; // the 20 is for scrollbar
+  videoPlayer.style.width = width + "px";
   videoPlayer.style.zIndex =  "999";
+  window.scroll(2000,0);
+  location.href = "#";
+  location.href = "#my_video_1";
+
+
   //document.body.scrollRight += 2000;
   console.log("video bloated");
 }
@@ -19,7 +24,3 @@ chrome.runtime.onMessage.addListener(
       sendResponse({farewell: "goodbye"});
     }
   });
-
-  function getScrollBarSize(){//hardcoded for now
-    return 17;
-  }
