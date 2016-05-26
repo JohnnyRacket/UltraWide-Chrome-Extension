6b844e11f54a2000c675ca13d72b9702024bcdfd
JohnnyRacket <john.randolph.harrison@gmail.com>
2016-05-25 22:43:16 -0400

    started to add youtube support

--- websites/youtube.js
+++ websites/youtube.js
@@ -1,17 +1,14 @@
-//weebs everywhere
 var WebsiteInformation = WebsiteInformation || {};
-WebsiteInformation.videoPlayer = document.getElementById("my_video_1");
+WebsiteInformation.videoPlayer = document.getElementsByClassName("player-api")[0];
 
-console.log("kissanime script loaded");
 function bloatVideo(width, height){
   setDefaults();
   WebsiteInformation.videoPlayer.style.height = height + "px"; // the 20 is for scrollbar
   WebsiteInformation.videoPlayer.style.width = width + "px";
   WebsiteInformation.videoPlayer.style.zIndex =  "999";
-
+  WebsiteInformation.videoPlayer.id = "my_video_1";
   movePage();
 
-  console.log("video bloated");
 }
 
 function movePage(){
@@ -24,3 +21,6 @@ function movePage(){
   window.scroll(findPosX(WebsiteInformation.videoPlayer),0);
   location.href = "#my_video_1"; //this is not graceful, but right now it has to exist unfortunately
 }
+
+//ytp-chrome-bottom for bottom bar
+//video object or video-stream html5-main-video classes for the video itself
